1fa8d4fbe16729fd2a44162c306a1b43ee1c8ed3
Ethan <ethansagin@gmail.com>
2019-02-23 19:36:31 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,12 +1,12 @@
 def valid_move?(board, index)
-  if index.between?(0, 8) && position_taken?(board, index) == 
+  if index.between?(0, 8) && position_taken?(board, index)
+    false 
+  elsif index.between?(0, 8)
     true
-  else
-    false
+  end
 end
 
 
-###
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
